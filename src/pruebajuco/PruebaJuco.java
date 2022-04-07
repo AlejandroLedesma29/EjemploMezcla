01a6071c0636ceb971860099481b2d49fcd2ff6a
AlejandroLedesma29 <ledesmazja@gmail.com>
2022-04-07 16:51:55 -0500

    Ejemplo1

---
+++ src/pruebajuco/PruebaJuco.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajuco;
+
+/**
+ *
+ * @author L E D E S M A
+ */
+public class PruebaJuco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
